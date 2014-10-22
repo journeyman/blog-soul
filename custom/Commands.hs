5628e714904349c73a8c70dd24d67644bc77995b
journeyman <journeymanmw@gmail.com>
2014-10-22 11:36:11 +0300

    added removeDirWithExceptions

--- custom/Commands.hs
+++ custom/Commands.hs
@@ -6,15 +6,31 @@ module Custom.Commands
 
 
 --------------------------------------------------------------------------------
-import           System.Directory (canonicalizePath)
-import           System.Exit      (ExitCode)
-import           System.FilePath  (isAbsolute, normalise, takeFileName)
-import           System.IO.Error  (catchIOError)
-import           System.Process   (system)
+import           System.Directory
+import           System.FilePath 
+import           Data.List
+import           Control.Monad
 
 import           Hakyll.Core.Configuration (Configuration, deploySite)
-
 
 --------------------------------------------------------------------------------
 deploy :: Configuration -> IO ExitCode
 deploy conf = deploySite conf conf
+
+removeDirWithExceptions :: FilePath -> [FilePath] -> IO Bool
+removeDirWithExceptions dirName excludeList = do
+    rawItems <- getDirectoryContents dirName
+    let afterExclusion = rawItems \\ excludeList
+    let didExclude = length rawItems /= length afterExclusion
+    let items = map (dirName </>) $ afterExclusion \\ dotDirs 
+    files <- filterM doesFileExist items
+    dirs <- filterM doesDirectoryExist items
+    mapM_ removeFile files
+    results <- mapM (flip removeDirWithExceptions excludeList) dirs
+    let exclusionsOccured = or results
+    let shouldRemove = not (didExclude || exclusionsOccured)
+    when shouldRemove (removeDirectory dirName)
+    return $ not shouldRemove
+
+dotDirs :: [FilePath]
+dotDirs = [".",".."]
